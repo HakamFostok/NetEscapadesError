d4d0db0eaa32044cbac482bead9082cc8a9e01f9
hfostok-plana <hfostok@planatechnologies.com>
2023-09-23 15:37:07 +0300

    Add project files.

---
+++ Application/JsonContext.cs
@@ -0,0 +1,12 @@
+﻿using NetEscapadesError;
+using System.Text.Json.Serialization;
+
+namespace Application;
+
+public class JsonModel
+{
+    public Enum1 Enum { get; set; }
+}
+
+[JsonSerializable(typeof(JsonModel))]
+public partial class MyJsonContext : JsonSerializerContext { }
---
+++ NetEscapadesError/Enum1.cs
@@ -0,0 +1,8 @@
+﻿namespace NetEscapadesError;
+
+//[NetEscapades.EnumGenerators.EnumExtensions]
+public enum Enum1
+{
+    Value1,
+    Value2,
+}
